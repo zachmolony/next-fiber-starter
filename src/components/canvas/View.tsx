94a2d62237048305f588354fed2cc1326a4bcbab
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2023-11-08 19:22:00 +0000

    feat: type 3d shit

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -1,10 +1,15 @@
 'use client'
 
-import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
+import { forwardRef, HTMLAttributes, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
 
-export const Common = ({ color }) => (
+type CommonProps = { color?: THREE.ColorRepresentation }
+type ViewProps = HTMLAttributes<HTMLDivElement> & {
+  orbit?: boolean
+}
+
+export const Common = ({ color }: CommonProps) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
     <ambientLight intensity={0.5} />
@@ -14,7 +19,7 @@ export const Common = ({ color }) => (
   </Suspense>
 )
 
-const View = forwardRef(({ children, orbit, ...props }, ref) => {
+const View = forwardRef<HTMLElement, ViewProps>(({ children, orbit, ...props }, ref) => {
   const localRef = useRef(null)
   useImperativeHandle(ref, () => localRef.current)
 
